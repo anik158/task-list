4ed87650c1f562651cfabe9ec6cc53f1d1e00e7f
Ahsanul Houqe <ahsanan@outlook.com>
2024-02-22 17:36:19 +0600

    added api

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,10 @@
 
     @section('content')
 
-        <nav class="mb-4 mt-4"><a class="link" href="{{route('tasks.create')}}">Add Task!</a></nav>
+        <nav class="mb-4 mt-4"><a class="btn link" href="{{route('tasks.create')}}">Add Task!</a></nav>
         @forelse($tasks as $task)
 <div>
-            <a @class(['line-through'=>$task->completed]) href="{{route('tasks.show',['task'=>$task->id])}}">{{$task->title}}</a>
+    💬<a @class(['text-xl','mb-4','line-through'=>$task->completed]) href="{{route('tasks.show',['task'=>$task->id])}}">{{$task->title}}</a>
 </div>
         @empty
         <div class="no-data">>No data found! </div>
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Get all tasks
+Route::get('/tasks', function () {
+    $tasks = Task::latest()->paginate(10);
+    return ['data' => $tasks];
+})->name('tasks.index');
+
+// Get a single task
+Route::get('/tasks/{task}', function (Task $task) {
+    return ['data' => $task];
+})->name('tasks.show');
+
+// Create a new task
+Route::post('/tasks', function (Request $request) {
+    $request->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = Task::create([
+        'title' => $request->title,
+        'description' => $request->description,
+        'long_description' => $request->long_description,
+    ]);
+
+    return [
+        'message' => 'Success',
+        'data' => [
+            'id' => $task->id,
+            'title' => $task->title,
+        ]
+    ];
+})->name('tasks.store');
+
+// Update a task
+Route::put('/tasks/{task}', function (Task $task, Request $request) {
+    $request->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task->update([
+        'title' => $request->title,
+        'description' => $request->description,
+        'long_description' => $request->long_description,
+    ]);
+
+    return [
+        'message' => 'Success',
+        'data' => [
+            'id' => $task->id,
+            'title' => $task->title,
+        ]
+    ];
+})->name('tasks.update');
+
+// Delete a task
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+    return ['message' => 'Success'];
+})->name('tasks.destroy');
+
+// Toggle task completion
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplete();
+    return [
+        'message' => 'Success',
+        'data' => [
+            'id' => $task->id,
+            'title' => $task->title,
+        ]
+    ];
+})->name('tasks.toggle-complete');
